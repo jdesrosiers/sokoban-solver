6bd18d240c96c2ea78d9a0120beaf3dbcf858168
Jason Desrosiers <jdesrosi@gmail.com>
2016-11-04 15:00:56 -0700

    Removed the need for a help function

--- src/main/scala/AI/Search.scala
+++ src/main/scala/AI/Search.scala
@@ -3,24 +3,20 @@ package ai
 import ai.frontier._
 
 object Search {
-  private def search[A](frontier: Frontier[A])(graph: Graph[A], start: Node[A]): Node[A] = {
-    def searchRec[A](frontier: Frontier[A], graph: Graph[A], current: Node[A], visited: Set[Node[A]]): Node[A] =
-      if (graph.isGoal(current))
-        current
-      else {
-        graph.children(current).filter(!visited.contains(_)).foreach(frontier.add(_))
-        if (frontier.hasNext)
-          searchRec(frontier, graph, frontier.next, visited + current)
-        else
-          null // No solution found
-      }
+  private def search[A](frontier: Frontier[A], visited: Set[Node[A]])(graph: Graph[A], current: Node[A]): Node[A] =
+    if (graph.isGoal(current))
+      current
+    else {
+      graph.children(current).filter(!visited.contains(_)).foreach(frontier.add(_))
+      if (frontier.hasNext)
+        search(frontier, visited + current)(graph, frontier.next)
+      else
+        null // No solution found
+    }
 
-    searchRec(frontier, graph, start, Set[Node[A]]())
-  }
-
-  def depthFirst[A] = search(new DepthFirstFrontier[A]()) _
-  def breadthFirst[A] = search(new BreadthFirstFrontier[A]()) _
-  def uniformCost[A] = search(new UniformCostFrontier[A]()) _
-  def greedyBestFirst[A] = search(new GreedyBestFirstFrontier[A]()) _
-  def aStar[A] = search(new AStarFrontier[A]()) _
+  def depthFirst[A] = search(new DepthFirstFrontier[A](), Set[Node[A]]()) _
+  def breadthFirst[A] = search(new BreadthFirstFrontier[A](), Set[Node[A]]()) _
+  def uniformCost[A] = search(new UniformCostFrontier[A](), Set[Node[A]]()) _
+  def greedyBestFirst[A] = search(new GreedyBestFirstFrontier[A](), Set[Node[A]]()) _
+  def aStar[A] = search(new AStarFrontier[A](), Set[Node[A]]()) _
 }
