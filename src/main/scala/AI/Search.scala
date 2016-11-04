9f9aaf492f1e5d0587282b9a24fe4a7b00c32601
Jason Desrosiers <jdesrosi@gmail.com>
2016-11-04 14:53:14 -0700

    Helper function doesn't need to be curried

--- src/main/scala/AI/Search.scala
+++ src/main/scala/AI/Search.scala
@@ -4,18 +4,18 @@ import ai.frontier._
 
 object Search {
   private def search[A](frontier: Frontier[A])(graph: Graph[A], start: Node[A]): Node[A] = {
-    def searchRec[A](frontier: Frontier[A])(graph: Graph[A], current: Node[A], visited: Set[Node[A]]): Node[A] =
+    def searchRec[A](frontier: Frontier[A], graph: Graph[A], current: Node[A], visited: Set[Node[A]]): Node[A] =
       if (graph.isGoal(current))
         current
       else {
         graph.children(current).filter(!visited.contains(_)).foreach(frontier.add(_))
         if (frontier.hasNext)
-          searchRec(frontier)(graph, frontier.next, visited + current)
+          searchRec(frontier, graph, frontier.next, visited + current)
         else
-          null
+          null // No solution found
       }
 
-    searchRec(frontier)(graph, start, Set[Node[A]]())
+    searchRec(frontier, graph, start, Set[Node[A]]())
   }
 
   def depthFirst[A] = search(new DepthFirstFrontier[A]()) _
