40d5372b8344754cf0002cb59dd1e2c4ab5e40d8
Jason Desrosiers <jdesrosi@gmail.com>
2016-11-19 11:12:51 -0800

    Cleaned restricted space code. Improved frozen.
    There are probably more improvements that could be made, but it's clean
    enough for now.
    
    I improved the frozen detection to include detection of being frozen
    by blocks as well as walls.  This made level 70 twice as fast and made
    level 65 finishable in a little under 3min.

--- src/main/scala/ai/sokoban/Game.scala
+++ src/main/scala/ai/sokoban/Game.scala
@@ -1,98 +1,59 @@
 package ai.sokoban
 
 case class Game(dimensions: Point, walls: Set[Point], storage: Set[Point]) {
-  // Corners
-  val corners: Set[Point] = for {
-    wall <- walls
-    (a, b) <- List('U -> 'R, 'U -> 'L, 'D -> 'R, 'D -> 'L)
-    if (isCorner(wall, a, b))
-  } yield wall.move(a).move(b)
+  val restricted = {
+    def isCorner(wall: Point, a: Symbol, b: Symbol) =
+      isWall(wall.move(a)) && isWall(wall.move(b)) && !isStorage(wall.move(a).move(b))
 
-  private def isCorner(wall: Point, a: Symbol, b: Symbol) =
-    isWall(wall.move(a)) && isWall(wall.move(b)) && !isStorage(wall.move(a).move(b))
+    def pathNeighborsWall(path: Seq[Point]) =
+      List('U, 'D, 'R, 'L).exists(direction => path.forall(p => isWall(p.move(direction))))
 
-  var restricted = corners
+    val corners = for {
+      wall <- walls
+      (a, b) <- List('U -> 'R, 'U -> 'L, 'D -> 'R, 'D -> 'L)
+      if (isCorner(wall, a, b))
+    } yield wall.move(a).move(b)
 
-  // Restrict points between corners along vertical walls
-  for (a <- corners; b <- corners; if (a.x == b.x)) yield {
-    val dir = if (a.y < b.y) 'D else 'U
-    // wall segment has a goal and wall is to the left
-    verticalRestricted(a, b, dir, 'L)
+    val horizontalCornerPaths = for {
+      a <- corners
+      b <- corners
+      path = for (x <- a.x to b.x) yield Point(x, a.y)
+      p <- path
+      if a.y == b.y && a.x < b.x && pathNeighborsWall(path) && !path.exists(isStorage)
+    } yield p
 
-    // wall segment has a goal and wall is to the right
-    verticalRestricted(a, b, dir, 'R)
-  }
+    val verticalCornerPaths = for {
+      a <- corners
+      b <- corners
+      path = for (y <- a.y to b.y) yield Point(a.x, y)
+      p <- path
+      if a.x == b.x && a.y < b.y && pathNeighborsWall(path) && !path.exists(isStorage)
+    } yield p
 
-  // Restrict points between corners along horizontal walls
-  for (a <- corners; b <- corners; if (a.y == b.y)) yield {
-    val dir = if (a.x < b.x) 'R else 'L
-    // wall segment has a goal and wall is above
-    horizontalRestricted(a, b, dir, 'U)
-
-    // wall segment has a goal and wall is to the right
-    horizontalRestricted(a, b, dir, 'D)
-  }
-
-  def verticalRestricted(a: Point, b: Point, dir: Symbol, direction: Symbol) = {
-    var hasGoal = false
-    var p = a
-    var leftWall = true
-    var rightWall = true
-    while (p.y != b.y) {
-      if (isStorage(p)) hasGoal = true
-      if (!isWall(p.move(direction))) leftWall = false
-      p = p.move(dir)
-    }
-
-    // restrict points on the wall segment
-    if (!hasGoal && leftWall) {
-      p = a
-      while (p.y != b.y) {
-        restricted += p
-        p = p.move(dir)
-      }
-    }
-
-    ()
-  }
-
-  def horizontalRestricted(a: Point, b: Point, dir: Symbol, direction: Symbol) = {
-    var hasGoal = false
-    var p = a
-    var upWall = true
-    var downWall = true
-    while (p.x != b.x) {
-      if (isStorage(p)) hasGoal = true
-      if (!isWall(p.move(direction))) upWall = false
-      p = p.move(dir)
-    }
-
-    // restrict points on the wall segment
-    if (!hasGoal && upWall) {
-      p = a
-      while (p.x != b.x) {
-        restricted += p
-        p = p.move(dir)
-      }
-    }
-
-    ()
+    corners ++ horizontalCornerPaths ++ verticalCornerPaths
   }
 
   def isWall(point: Point) = walls contains point
   def isStorage(point: Point) = storage contains point
   def isRestricted(point: Point) = restricted contains point
 
-  private def isFrozen(state: GameState, point: Point , a: Symbol, b: Symbol) =
-    isWall(point.move(a)) && state.isBox(point.move(b)) && isWall(point.move(a).move(b))
-
   def canMove(state: GameState, direction: Symbol) = {
+    def isFrozen(state: GameState, point: Point , a: Symbol, b: Symbol) =
+      (isWall(point.move(a)) || state.isBox(point.move(a))) &&
+      (isWall(point.move(b)) || state.isBox(point.move(b))) &&
+      (isWall(point.move(a).move(b)) || state.isBox(point.move(a).move(b)))
+
     val destination = state.player.move(direction)
 
     if (isWall(destination)) false
     else if (state.isBox(destination)) {
       val boxDestination = destination.move(direction)
-      val frozen = !isStorage(boxDestination) && List('U, 'D, 'L, 'R).exists(isFrozen(state, boxDestination, direction, _))
+      val frozen = !isStorage(boxDestination) && (direction match {
+        case 'U => List('L, 'R).exists(isFrozen(state, boxDestination, direction, _))
+        case 'D => List('L, 'R).exists(isFrozen(state, boxDestination, direction, _))
+        case 'R => List('U, 'D).exists(isFrozen(state, boxDestination, direction, _))
+        case 'L => List('U, 'D).exists(isFrozen(state, boxDestination, direction, _))
+      })
 
       !isWall(boxDestination) && !state.isBox(boxDestination) && !isRestricted(boxDestination) && !frozen
     } else true
--- src/test/scala/ai/sokoban/GameSpec.scala
+++ src/test/scala/ai/sokoban/GameSpec.scala
@@ -5,11 +5,25 @@ import org.scalatest._
 import scala.io.Source
 
 class GameSpec extends FlatSpec with Matchers {
-  "restricted points" should "be in restricted" in {
+  "restricted points in level 3" should "be in restricted" in {
     val initializer = new Initializer(Source.fromFile("resources/level03"))
     val game = initializer.game
 
-    game.restricted should be (Set(Point(3, 4), Point(4, 1), Point(4, 3), Point(4, 2), Point(2, 4), Point(4, 4)))
+    game.restricted should be (Set(Point(3, 4), Point(4, 1), Point(4, 3), Point(2, 4), Point(4, 4)))
+  }
+
+  "restricted points in level 15" should "be in restricted" in {
+    val initializer = new Initializer(Source.fromFile("resources/level15"))
+    val game = initializer.game
+
+    game.restricted should be (Set(Point(7,5), Point(2,5), Point(5,2), Point(3,4), Point(4,7), Point(6,2), Point(6,3), Point(2,6), Point(5,7), Point(1,3), Point(2,2), Point(2,7), Point(4,2), Point(3,7)))
+  }
+
+  "restricted points in level 20" should "be in restricted" in {
+    val initializer = new Initializer(Source.fromFile("resources/level20"))
+    val game = initializer.game
+
+    game.restricted should be (Set(Point(1,8), Point(2,5), Point(5,2), Point(6,8), Point(2,6), Point(3,2), Point(2,2), Point(2,4), Point(3,7), Point(2,3), Point(5,6)))
   }
 
   "restricted points" should "not be in allowed moves" in {
