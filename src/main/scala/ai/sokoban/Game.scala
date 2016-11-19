30e2824ad19affd754432ce528f0e361b753448b
Jason Desrosiers <jdesrosi@gmail.com>
2016-11-19 03:19:28 -0800

    Some cleanup of restricted cell generation

--- src/main/scala/ai/sokoban/Game.scala
+++ src/main/scala/ai/sokoban/Game.scala
@@ -2,7 +2,7 @@ package ai.sokoban
 
 case class Game(dimensions: Point, walls: Set[Point], storage: Set[Point]) {
   // Corners
-  var restricted: Set[Point] = for {
+  val corners: Set[Point] = for {
     wall <- walls
     (a, b) <- List('U -> 'R, 'U -> 'L, 'D -> 'R, 'D -> 'L)
     if (isCorner(wall, a, b))
@@ -11,95 +11,73 @@ case class Game(dimensions: Point, walls: Set[Point], storage: Set[Point]) {
   private def isCorner(wall: Point, a: Symbol, b: Symbol) =
     isWall(wall.move(a)) && isWall(wall.move(b)) && !isStorage(wall.move(a).move(b))
 
+  var restricted = corners
+
   // Restrict points between corners along vertical walls
-  restricted.foreach(a =>
-    restricted.foreach(b => {
-      val dir = if (a.y < b.y) 'D else 'U
-      if (a.x == b.x) {
-        // wall segment has a goal and wall is to the left
-        var hasGoal = false
-        var p = a
-        var leftWall = true
-        var rightWall = true
-        while (p.y != b.y) {
-          if (isStorage(p)) hasGoal = true
-          if (!isWall(p.move('L))) leftWall = false
-          p = p.move(dir)
-        }
+  for (a <- corners; b <- corners; if (a.x == b.x)) yield {
+    val dir = if (a.y < b.y) 'D else 'U
+    // wall segment has a goal and wall is to the left
+    verticalRestricted(a, b, dir, 'L)
 
-        // restrict points on the wall segment
-        if (!hasGoal && leftWall) {
-          p = a
-          while (p.y != b.y) {
-            restricted += p
-            p = p.move(dir)
-          }
-        }
-
-        // wall segment has a goal and wall is to the right
-        p = a
-        while (p.y != b.y) {
-          if (isStorage(p)) hasGoal = true
-          if (!isWall(p.move('R))) rightWall = false
-          p = p.move(dir)
-        }
-
-        // restrict points on the wall segment
-        if (!hasGoal && rightWall) {
-          p = a
-          while (p.y != b.y) {
-            restricted += p
-            p = p.move(dir)
-          }
-        }
-      }
-    })
-  )
+    // wall segment has a goal and wall is to the right
+    verticalRestricted(a, b, dir, 'R)
+  }
 
   // Restrict points between corners along horizontal walls
-  restricted.foreach(a =>
-    restricted.foreach(b => {
-      val dir = if (a.x < b.x) 'R else 'L
-      if (a.y == b.y) {
-        // wall segment has a goal and wall is above
-        var hasGoal = false
-        var p = a
-        var upWall = true
-        var downWall = true
-        while (p.x != b.x) {
-          if (isStorage(p)) hasGoal = true
-          if (!isWall(p.move('U))) upWall = false
-          p = p.move(dir)
-        }
+  for (a <- corners; b <- corners; if (a.y == b.y)) yield {
+    val dir = if (a.x < b.x) 'R else 'L
+    // wall segment has a goal and wall is above
+    horizontalRestricted(a, b, dir, 'U)
 
-        // restrict points on the wall segment
-        if (!hasGoal && upWall) {
-          p = a
-          while (p.x != b.x) {
-            restricted += p
-            p = p.move(dir)
-          }
-        }
+    // wall segment has a goal and wall is to the right
+    horizontalRestricted(a, b, dir, 'D)
+  }
 
-        // wall segment has a goal and wall is to the right
-        p = a
-        while (p.x != b.x) {
-          if (isStorage(p)) hasGoal = true
-          if (!isWall(p.move('D))) downWall = false
-          p = p.move(dir)
-        }
+  def verticalRestricted(a: Point, b: Point, dir: Symbol, direction: Symbol) = {
+    var hasGoal = false
+    var p = a
+    var leftWall = true
+    var rightWall = true
+    while (p.y != b.y) {
+      if (isStorage(p)) hasGoal = true
+      if (!isWall(p.move(direction))) leftWall = false
+      p = p.move(dir)
+    }
 
-        // restrict points on the wall segment
-        if (!hasGoal && downWall) {
-          p = a
-          while (p.x != b.x) {
-            restricted += p
-            p = p.move(dir)
-          }
-        }
+    // restrict points on the wall segment
+    if (!hasGoal && leftWall) {
+      p = a
+      while (p.y != b.y) {
+        restricted += p
+        p = p.move(dir)
       }
-    })
-  )
+    }
+
+    ()
+  }
+
+  def horizontalRestricted(a: Point, b: Point, dir: Symbol, direction: Symbol) = {
+    var hasGoal = false
+    var p = a
+    var upWall = true
+    var downWall = true
+    while (p.x != b.x) {
+      if (isStorage(p)) hasGoal = true
+      if (!isWall(p.move(direction))) upWall = false
+      p = p.move(dir)
+    }
+
+    // restrict points on the wall segment
+    if (!hasGoal && upWall) {
+      p = a
+      while (p.x != b.x) {
+        restricted += p
+        p = p.move(dir)
+      }
+    }
+
+    ()
+  }
 
   def isWall(point: Point) = walls contains point
   def isStorage(point: Point) = storage contains point
