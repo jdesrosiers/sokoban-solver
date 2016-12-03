6e3daa03f89b9d9314ff58f2ceeabcf1bc5eb6d6
Jason Desrosiers <jdesrosi@gmail.com>
2016-12-02 23:03:00 -0800

    Add class levels

--- src/main/scala/ai/search/Node.scala
+++ src/main/scala/ai/search/Node.scala
@@ -1,17 +1,21 @@
 package ai.search
 
+import scala.annotation.tailrec
+
 case class Node[A](parent: Node[A], operation: Symbol, state: A, g: Double, h: Double) {
   val f = g + h
   val depth: Int = if (parent == null) 1 else parent.depth + 1
 
   def states: List[A] = states(Nil)
-  def states(acc: List[A]): List[A] = parent match {
+  @tailrec
+  private def states(acc: List[A]): List[A] = parent match {
     case null => state :: acc
     case _ => parent.states(state :: acc)
   }
 
   def operations: List[Symbol] = operations(Nil)
-  def operations(acc: List[Symbol]): List[Symbol] = parent match {
+  @tailrec
+  private def operations(acc: List[Symbol]): List[Symbol] = parent match {
     case null => acc
     case _ => parent.operations(operation :: acc)
   }
